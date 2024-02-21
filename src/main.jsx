bb1b07f7518c8179dc4cb4fd2eb9b6eed18da421
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-21 23:12:29 +0100

    edit: boh non so

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <ThemeProvider>
       <App />
-    </HashRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
