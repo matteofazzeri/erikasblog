09a3c6c3ad0824064b03844d2444e0c67aa43616
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-21 23:47:53 +0100

    edit: vediamo se così va

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/erikasblog">
       <App />
     </HashRouter>
   </React.StrictMode>
