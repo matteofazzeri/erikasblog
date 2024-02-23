edfac1986bc7077778c02706b77af0f5973a8741
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-23 23:41:51 +0100

    test: vercel test2

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import { ThemeProvider } from "@material-tailwind/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-        <App />
+      <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
