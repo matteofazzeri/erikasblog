e0e1529434bef682c24970c7fb48299e092c909d
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-23 23:43:22 +0100

    test: vercel test3

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ThemeProvider } from "@material-tailwind/react";
 
