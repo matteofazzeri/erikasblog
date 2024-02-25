a549034f7ed61f93a285f7c96a0554adc85b7768
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-25 16:19:27 +0100

    edit: routing change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminPanel from "./pages/AdminPanel";
 
 export default function App() {
-  return <main className="w-screen min-h-screen">CIAO</main>;
+  return (
+    <main className="w-screen min-h-screen">
+      <Router>
+        <Routes>
+          <Route path="/erikasblog" element={<Home />} />
+          <Route path="/erikasblog/admin/*" element={<AdminPanel />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -55,7 +55,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
