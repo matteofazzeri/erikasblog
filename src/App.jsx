01779f17d959f1df9e5e606f3b8b2cec912fa934
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-26 23:10:41 +0100

    test: redirecting with vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Link,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import AdminPanel from "./pages/AdminPanel";
 import NotFound from "./pages/NotFound";
