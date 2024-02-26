f202d9897fecdf0e7ecb3a946ebb29e0bd008cd7
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-02-26 19:33:09 +0100

    test: routing for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import AdminPanel from "./pages/AdminPanel";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import PrivateRoutes from "./auth/PrivateRoute";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App() {
   return (
     <main className="min-h-screen">
+      <Analytics />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route element={<PrivateRoutes />}>
             <Route path="admin/*" element={<AdminPanel />} />
+            <Route path="admin/posts" element={<AdminPanel />} />
+            <Route path="admin/categories" element={<AdminPanel />} />
           </Route>
           <Route path="login" element={<Login />} />
           <Route path="" element={<NotFound />} />
